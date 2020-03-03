39e2ffac3e0bc8706638f0304e6f3c5b31187e2f
MrR3set <bairon.pazrobles@gmail.com>
2020-03-03 14:25:01 -0800

    some fixes

--- silent-auction/src/components/BidderDash.js
+++ silent-auction/src/components/BidderDash.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -8,11 +7,18 @@ import Button from '@material-ui/core/Button';
 import {NavLink,Route} from "react-router-dom";
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import { makeStyles} from '@material-ui/core/styles';
+
+let mainColor="MEDIUMSLATEBLUE"
+let secondaryColor="white"
+let mainColorHover="DARKSLATEBLUE"
+
+
 
 const useStyles = makeStyles(theme => ({
     root: {
         background:"#262626",
-        color:"white",
+        color:secondaryColor,
         borderRadius:"15px",
         margin:"0 auto",
         maxWidth:"80%",
@@ -29,12 +35,27 @@ const useStyles = makeStyles(theme => ({
         alignContent:"center",
         flexDirection:"column",
         paddingBottom:"6%",
-        borderRight:"1px solid #2e2d2d"
+        borderRight:"1px solid #2e2d2d",        
+        "& h1":{
+            fontFamily:"Nunito, sans-serif",
+            fontSize:"2rem",
+            fontWeight:700
+        },        
+        "& p":{
+            fontFamily:"Roboto, sans-serif",
+            fontSize:"1.2rem"
+        }
     },
     lastChld:{
         marginTop:"auto",
         alignSelf:"center",
-        width:"50%"
+        width:"50%",
+        background:mainColor,
+        transition:"all 0.5s ease-in-out",
+        fontFamily:"Roboto,sans-serif",
+        '&:hover':{
+            background:mainColorHover
+        }
     },
     media:{
         width:"65%",
@@ -53,11 +74,14 @@ const useStyles = makeStyles(theme => ({
         justifyContent:"space-around",
         '& a':{
             textTransform:"uppercase",
-            color:"white",
+            color:mainColor,
+            fontWeight:700,
             textDecoration:"none",
             fontSize:"1.5rem",
+            fontFamily:"Nunito, sans-serif",
+            transition:"all 0.5s ease-in-out",
             '&:hover':{
-                
+                color:mainColorHover,
             }
         }
     },
@@ -66,7 +90,7 @@ const useStyles = makeStyles(theme => ({
         overflow:"hidden",
         display:"flex",
         flexWrap:"wrap",
-        justifyContent:"space-between",
+        justifyContent:"space-evenly",
         '&:hover':{
             overflow:"auto"
         },
@@ -74,18 +98,39 @@ const useStyles = makeStyles(theme => ({
     auctionCard:{
         height:"250px",
         width:"175px",
-        marginBottom:"3%",        
+        marginBottom:"3%",
+        position:"relative",        
         '& img':{
             objectFit:"cover",
             width:"100%",
             height:"100%",
-            filter: "grayscale(80%)",
-            blur:"15px",
+            filter: "grayscale(0%)",           
             transition: "all 0.5s ease-in-out",
             '&:hover':{
-                filter: "grayscale(0%)",
-                blur:"0px",
             }
+        },
+        "& div":{
+            position:"absolute",
+            top:0,
+            left:0,
+            height:"80%",
+            width:"80%",
+            color:secondaryColor,
+            fontWeight:"700",
+            textTransform:"uppercase",
+            display:"none"
+
+        },
+        "&:hover div":{
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            alignContent:"center"
+        },
+        "&:hover img":{
+            filter: "grayscale(100%) blur(1px) brightness(50%)",
+            blur:"0px",
+            cursor:"pointer"
         },
         '&:last-child':{
             display:"flex",
@@ -93,7 +138,11 @@ const useStyles = makeStyles(theme => ({
             justifyContent:"center",
             alignItems:"center",
             '& button':{ 
-                background:"#f50057"
+                background:mainColor,
+                transition:"all 0.5s ease-in-out",
+                '&:hover':{
+                    background:mainColorHover
+                }
             }
         }
     }
@@ -113,7 +162,7 @@ export default function SellerDash() {
                     />
                     User name in here
                 </Typography>
-                <Typography gutterBottom variant="subtitle1" component="h1">
+                <Typography gutterBottom variant="body1" component="p">
                     Account type
                 </Typography>
                 <Button variant="contained" color="secondary" className={classes.lastChld}>
@@ -136,42 +185,28 @@ export default function SellerDash() {
                                 image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                                 title="Contemplative Reptile"
                                 component="img"/>
+                            <CardContent>
+                            <Typography gutterBottom variant="subtitle1" component="p">
+                                Title here
+                            </Typography>
+                            <Typography gutterBottom variant="subtitle1" component="p">
+                                Checkout
+                            </Typography>
+                            </CardContent>
                         </Card>
                         <Card className={classes.auctionCard}>
                             <CardMedia
                                 image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                                 title="Contemplative Reptile"
                                 component="img"/>
-                        </Card>
-                        <Card className={classes.auctionCard}>
-                            <CardMedia
-                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                title="Contemplative Reptile"
-                                component="img"/>
-                        </Card>
-                        <Card className={classes.auctionCard}>
-                            <CardMedia
-                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                title="Contemplative Reptile"
-                                component="img"/>
-                        </Card>
-                        <Card className={classes.auctionCard}>
-                            <CardMedia
-                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                title="Contemplative Reptile"
-                                component="img"/>
-                        </Card>
-                        <Card className={classes.auctionCard}>
-                            <CardMedia
-                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                title="Contemplative Reptile"
-                                component="img"/>
-                        </Card>
-                        <Card className={classes.auctionCard}>
-                            <CardMedia
-                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                                title="Contemplative Reptile"
-                                component="img"/>
+                            <CardContent>
+                            <Typography gutterBottom variant="subtitle1" component="p">
+                                Title here
+                            </Typography>
+                            <Typography gutterBottom variant="subtitle1" component="p">
+                                Checkout
+                            </Typography>
+                            </CardContent>
                         </Card>
                         {/* {Hard codded elements} */}
                         <Card className={classes.auctionCard}>
@@ -194,13 +229,104 @@ export default function SellerDash() {
                                 image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                                 title="Contemplative Reptile"
                                 component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
                         </Card>
                         <Card className={classes.auctionCard}>
                             <CardMedia
                                 image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
                                 title="Contemplative Reptile"
                                 component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
                         </Card>
+                        <Card className={classes.auctionCard}>
+                            <CardMedia
+                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                title="Contemplative Reptile"
+                                component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
+                        </Card>
+                        <Card className={classes.auctionCard}>
+                            <CardMedia
+                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                title="Contemplative Reptile"
+                                component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
+                        </Card>
+                        <Card className={classes.auctionCard}>
+                            <CardMedia
+                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                title="Contemplative Reptile"
+                                component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
+                        </Card>
+                        <Card className={classes.auctionCard}>
+                            <CardMedia
+                                image="https://images.pexels.com/photos/175568/pexels-photo-175568.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                                title="Contemplative Reptile"
+                                component="img"/>
+                                <CardContent>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Title here
+                                    </Typography>
+                                    <Typography gutterBottom variant="subtitle1" component="p">
+                                        Checkout
+                                    </Typography>
+                                    <Typography gutterBottom variant="caption" component="p">
+                                        Date ended
+                                    </Typography>
+                                </CardContent>
+                        </Card>
+
                     </Route>
                 </CardContent>
             </CardContent>
