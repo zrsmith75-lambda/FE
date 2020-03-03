caded592d63af761eb86b3bb7dca7a6794a88863
MrR3set <bairon.pazrobles@gmail.com>
2020-03-03 09:34:38 -0800

    minor fixes

---
+++ silent-auction/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Components
+import {AuctionContainer} from './components/AuctionContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <AuctionContainer/>
+    </div>
+  );
+}
+
+export default App;
