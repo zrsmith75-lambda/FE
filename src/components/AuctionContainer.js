67309977d91380f417ec2181ad0a498596437a74
Zac Smith <zrs3141592@gmail.com>
2020-03-04 18:24:08 -0700

    testing 4

--- src/components/AuctionContainer.js
+++ src/components/AuctionContainer.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import {Switch, Route} from 'react-router-dom'
-import PrivateRoute from '../utils/PrivateRoute'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "../utils/PrivateRoute";
 
-import Navigation from './Navigation'
+import Navigation from "./Navigation";
 // import AuctionPage from './AuctionPage'
-import Login from './Login'
-import Signup from './Signup'
-import SellerDash from './SellerDash'
-import BidderDash from './BidderDash'
-import AuctionList from './AuctionList'
-import CloserLook from './CloserLook'
+import Login from "./Login";
+import Signup from "./Signup";
+import SellerDash from "./SellerDash";
+import BidderDash from "./BidderDash";
+import AuctionList from "./AuctionList";
+import CloserLook from "./CloserLook";
 
 export const AuctionContainer = () => {
+  const token = window.localStorage.getItem("token");
 
-    const token = window.localStorage.getItem('token')
-    
-    return (
-        <div>
-            <Navigation/>
-            <Switch>
-                <Route exact path='/signup' component={Signup}/>
-                <Route exact path='/login' component={Login}/>
-                <PrivateRoute exact path='/auctions' component={AuctionList}/>
-                <PrivateRoute exact path='/dashboard/seller/:id' component={SellerDash}/>
-                <PrivateRoute exact path='/dashboard/bidder/:id' component={BidderDash}/>
-                <PrivateRoute exact path='/closerlook/:id' component={CloserLook}/>
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navigation />
+      <Switch>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/auctions" component={AuctionList} />
+        <PrivateRoute
+          exact
+          path="/dashboard/seller/:id"
+          component={SellerDash}
+        />
+        <PrivateRoute
+          exact
+          path="/dashboard/bidder/:id"
+          component={BidderDash}
+        />
+        <PrivateRoute exact path="/closerlook/:id" component={CloserLook} />
+        <p>Auction Cointainer</p>
+      </Switch>
+    </div>
+  );
+};
